6366467d7523152f44c3d8b88e6c2427017bded9
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-10-07 19:01:12 +0200

    Creacion deonbjetos parte 2

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ if __name__ == '__main__':
     persona1 = Persona()  ## Crea un objeto vacio en memoria
     persona1.inicializar_persona('Laiai', 'Acosta')
     persona1.mostrar_persona()
+
+    ##Creacin de un segundo objeto
+    persona2 = Persona()
+    persona2.inicializar_persona('Ian','Sanchez')
+    persona2.mostrar_persona()
