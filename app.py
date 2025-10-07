1de05cdbf1050fa81072a9c18094300e2dc43d26
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-10-07 18:36:20 +0200

    Creacion de objetos en python

---
+++ app.py
@@ -0,0 +1,7 @@
+from persona import Persona
+
+if __name__ == '__main__':
+    # Creacion de un primer objeto
+    persona1 = Persona()  ## Crea un objeto vacio en memoria
+    persona1.inicializar_persona('Laiai', 'Acosta')
+    persona1.mostrar_persona()
